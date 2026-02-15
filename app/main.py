e04ff9e4a674b0d009e667a58744aa189ecaf35d
izamaeta <ahmetata321@gmail.com>
2026-02-15 16:35:46 +0300

    implement DELETE /posts/{id} using SQL

--- app/main.py
+++ app/main.py
@@ -82,11 +82,13 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", str((id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if delete_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} is not exist")
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
