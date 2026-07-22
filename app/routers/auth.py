19c6816814cd8e3b3f51867f57bc0aa1f3d303a2
izamaeta <ahmetata321@gmail.com>
2026-07-22 22:57:52 +0300

    add JWT access token creation on login (oauth2.py)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session 
 
-from ..import database, schemas, models, utils
+from ..import database, schemas, models, utils, oauth2
 router = APIRouter(tags= ["Authentication"])
 
 @router.post('/login')
@@ -17,4 +17,6 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
 
     #create token
     #return token
-    return {"token": "exmp token"}
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id}) 
+    return {"access_token": access_token, "token_type": "bearer"}
