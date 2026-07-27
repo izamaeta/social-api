02db9f11a870756c46272fd24ba6f070307842d3
izamaeta <ahmetata321@gmail.com>
2026-07-27 11:43:10 +0300

    add vote counts to posts via SQLAlchemy join and update response schema

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,18 +3,25 @@ from .. import models, schemas, oauth2
 from typing import List, Optional
 from sqlalchemy.orm import Session
 from ..database import get_db
+from sqlalchemy import func
 
 router = APIRouter(
     prefix="/posts",
     tags=['Posts']
 
 )
-
-@router.get("/", response_model=List[schemas.Post])
-async def get_posts(db: Session = Depends(get_db), 
-                    limit: int = 10, skip: int = 0, search: Optional[str] = ""):    #cursor.execute("""SELECT * FROM posts""")
+# @router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
+            limit: int = 10, skip: int = 0, search: Optional[str] = ""):    #cursor.execute("""SELECT * FROM posts""")
     #posts = cursor.fetchall()
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()    
+    #posts = db.query(models.Post).filter(
+    #    models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).filter(
+        models.Post.title.contains(search)).group_by(models.Post.id).limit(limit).offset(skip).all()
+
     return posts
 
 
@@ -38,16 +45,17 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current
     return new_post
 
 
-
-
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db)):
     #cursor.execute("""SELECT * FROM posts WHERE id = %s""", str((id),))
     #post = cursor.fetchone()
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    print(post)
+    #post = db.query(models.Post).filter(models.Post.id == id).first()
+    #print(post)
 
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True
+    ).filter(models.Post.id == id).group_by(models.Post.id).first()
 
     if not post: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
