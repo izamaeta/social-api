87b1a7043f42af64e1b4ff3ce9c2ac18596811c6
izamaeta <ahmetata321@gmail.com>
2026-08-03 13:27:25 +0300

    add test database setup and test for user creation

---
+++ tests/test_users.py
@@ -0,0 +1,10 @@
+from app import schemas
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
